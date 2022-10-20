930c4e5f62df3b757b5faa3f4071b2075fd93d67
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-10-20 18:23:22 +0530

    Create Contacts in Address Book Added

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package addressbooksystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+	List<PersonDetails> contactList = new ArrayList<PersonDetails>();
+}
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,7 +4,9 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 
+		AddressBook addressBook = new AddressBook();
 		System.out.println("Welcome to Address Book Program");
+		System.out.println(addressBook.contactList);
 	}
 
 }
