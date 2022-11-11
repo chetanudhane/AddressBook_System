8b3eeb00e12523ce886628eb2b3a2a392417697a
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-11-11 16:24:35 +0530

    CSV File Added

--- AddressBookDirectoryInterface.java
+++ AddressBookDirectoryInterface.java
@@ -1,4 +1,4 @@
-package addressbooksystem;
+package com.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,20 +6,12 @@ import java.util.HashMap;
 public interface AddressBookDirectoryInterface {
 
 	public void operationDirectory();
-
 	public void addAddressBook();
-
 	public void editAddressBook();
-
 	public void displayDirectoryDetails();
-
-	public void searchPersonByCity();
-
+	public void	searchPersonByCity();
 	public void searchPersonByState();
-
-	public void displayPeopleByRegion(HashMap<String, ArrayList<PersonDetails>> listToDisplay);
-
+	public void displayPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
 	public void countPeopleByCity();
-
 	public void countPeopleByState();
 }
